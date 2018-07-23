304ef5a0a93daac3c13386589e9f664396aeec57
gaurav <charpegaurav9@gmail.com>
2018-07-23 12:51:24 +0700

    updated datset

---
+++ Label-Image/Extracted_data/Images/Readme.txt
@@ -0,0 +1,5 @@
+
+Script main.py requires directory name should be integer
+
+Directories: 
+gmsl_2018-04-10-08-28-25.bag topic=/camera_a3 --> 001
---
+++ Label-Image/scripts/resize.py
@@ -0,0 +1,22 @@
+import os
+import getopt
+import sys
+from PIL import Image
+
+width = 600
+height = 600
+directory = '/home/arc/Deep_learning/Label-Image/Extracted_data/Images/001/'
+
+for image in os.listdir(directory):
+    print('Resizing image ' + image)
+ 
+    # Open the image file.
+    img = Image.open(os.path.join(directory, image))
+ 
+    # Resize it.
+    img = img.resize((width, height), Image.BILINEAR)
+ 
+    # Save it back to disk.
+    img.save(os.path.join(directory,image))
+ 
+print('Batch processing complete.')
